885901db4e315df1e73d6e3cbb6ed65c79b076cf
carrod23daw2l <15587030.clot@fje.edu>
2024-03-03 23:03:08 +0100

    ended get method substracted for HS

--- js/querySubstractor.js
+++ js/querySubstractor.js
@@ -1,6 +1,11 @@
-function substract() {
+function substract(query) {
 
-    const queryString = window.location.search;
+    if (query == ''){
+        console.log("sdfgg")
+        query = window.location;
+    }
+    var url = new URL(query)
+    const queryString = url.searchParams;
     const urlParams = new URLSearchParams(queryString);
 
     // Get all keys (parameter names)
