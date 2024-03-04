fb26afc88c79279f4619dc04b3d25b6a4103a0eb
carrod23daw2l <15587030.clot@fje.edu>
2024-03-04 03:12:12 +0100

    added copy button function

--- js/copyButton.js
+++ js/copyButton.js
@@ -1,4 +1,12 @@
-window.onload = () => {
-    const buttons = [];
-    
+function copyCompute()  {
+    const buttons = document.getElementsByClassName("copy");
+
+    for (var element of buttons){
+        var id = element.id;
+        id = id.split("@")[1];
+        element.addEventListener("click", () => {
+            var cosita = document.getElementById(id).innerHTML;
+            navigator.clipboard.writeText(cosita);
+        })
+    }
 }
